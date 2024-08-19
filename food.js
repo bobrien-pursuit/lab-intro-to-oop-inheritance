5270bfe5f2cf3d6a51054342cffcb625f0e71bab
Brendan O'Brien <bobrien@pursuit.org>
2024-08-19 16:56:26 -0400

    finishes food class

--- food.js
+++ food.js
@@ -1,4 +1,21 @@
 // Create class below
-
+class Food{
+    constructor(name,  daysToSpoil, fresh=true){
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = fresh;
+    }
+    prepare() {
+        console.log(`${this.name} is being prepared.`);
+    }
+    isFresh(){
+        console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils`);
+    }
+    aDayPasses(){
+        this.daysToSpoil--;
+        this.isFresh();
+    }
+}
 // Do not edit below this line
 module.exports = Food;
+
